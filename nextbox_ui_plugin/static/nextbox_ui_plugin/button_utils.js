f8a2235c38083f0f7b0be3d837eddc51df926945
iDebugAll <32979193+iDebugAll@users.noreply.github.com>
2020-05-31 11:45:51 +0300

    Implement visibility control for passive devices (patch panels, PDUs) (#7)
    * Implement visibility control for passive devices
    Passive devices (patch pannels, PDUs) visibility can now be controlled separately with DISPLAY_PASSIVE_DEVICES plugin parameter.
    Device is considered passive if it has cables connected to Front and Rear Ports only and not to Interfaces.
    Passive devices are hidden by default. You can display them with 'Display Passive Devices' button on the topology view page.
    While the passive devices are hidden a logical direct links between the actual end-devices are being displayed. With DISPLAY_LOGICAL_MULTICABLE_LINKS plugin parameter, you can display this link initially on the topology view along with the actual cabling in case if passive devices are visible.
    
    * Update README.md
    
    * Update sample images
    
    Co-authored-by: Debug All <iDebugAll@gmail.com>

--- nextbox_ui_plugin/static/nextbox_ui_plugin/button_utils.js
+++ nextbox_ui_plugin/static/nextbox_ui_plugin/button_utils.js
@@ -19,3 +19,24 @@ function showHideUndonnectedButtonOnClick(button) {
 function layerSelectorOnChange(checkbox){
     showHideDeviceRoles(checkbox.value, checkbox.checked);
 };
+
+function showHidePassiveDevicesButtonInitial() {
+    showHidePassiveDevicesButton = document.getElementById("showHidePassiveDevicesButton");
+    if (displayPassiveDevices == false) {
+        showHidePassiveDevicesButton.value = 'Display Passive Devices'
+    } else {
+        showHidePassiveDevicesButton.value = 'Hide Passive Devices';
+    };
+};
+
+function showHidePassiveDevicesButtonOnClick(button) {
+    if (button.value == 'Hide Passive Devices') {
+        button.value = 'Display Passive Devices'
+        displayLogicalMultiCableLinks = true;
+        showHideLogicalMultiCableLinks();
+    } else {
+        button.value = 'Hide Passive Devices';
+        showHideLogicalMultiCableLinks();
+    };
+    showHidePassiveDevices();
+};
