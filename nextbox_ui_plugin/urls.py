e7bf34928ac1017aa16852b480be051d23944bca
Debug All <iDebugAll@gmail.com>
2020-06-08 19:18:40 +0300

    Fix site topology button url Now renders using Django url template.

--- nextbox_ui_plugin/urls.py
+++ nextbox_ui_plugin/urls.py
@@ -2,5 +2,5 @@ from django.urls import path
 from . import views
 
 urlpatterns = [
-    path('site_topology/<int:site_id>/', views.TopologyView.as_view()),
+    path('site_topology/<int:site_id>/', views.TopologyView.as_view(), name='site_topology'),
 ]
