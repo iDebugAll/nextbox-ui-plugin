86cb015fc926e46b5dc928fe25a0cce821331f37
Debug All <iDebugAll@gmail.com>
2020-12-12 12:41:08 +0300

    Fix plugin description

--- nextbox_ui_plugin/__init__.py
+++ nextbox_ui_plugin/__init__.py
@@ -3,8 +3,8 @@ from extras.plugins import PluginConfig
 class NextBoxUIConfig(PluginConfig):
     name = 'nextbox_ui_plugin'
     verbose_name = 'NextBox UI'
-    description = 'Test'
-    version = '0.6.6'
+    description = 'A topology visualization plugin for Netbox powered by NextUI Toolkit.'
+    version = '0.6.7'
     author = 'Igor Korotchenkov'
     author_email = 'iDebugAll@gmail.com'
     base_url = 'nextbox-ui'
