f5c544a445e712040b52e7df07af23eeef8e26ea
Igor Korotchenkov <idebugall@gmail.com>
2022-12-21 21:11:18 +0400

    v0.11.0

--- nextbox_ui_plugin/__init__.py
+++ nextbox_ui_plugin/__init__.py
@@ -4,7 +4,7 @@ class NextBoxUIConfig(PluginConfig):
     name = 'nextbox_ui_plugin'
     verbose_name = 'NextBox UI'
     description = 'A topology visualization plugin for Netbox powered by NextUI Toolkit.'
-    version = '0.10.0'
+    version = '0.11.0'
     author = 'Igor Korotchenkov'
     author_email = 'iDebugAll@gmail.com'
     base_url = 'nextbox-ui'
