6ae0a28a6f0f175ccaa07dac6cd186a5f19a99a8
Daniel Sheppard <dans@dansheps.com>
2024-03-06 23:22:16 -0600

    Fix Name & Description

--- netbox_topology_plugin/__init__.py
+++ netbox_topology_plugin/__init__.py
@@ -5,8 +5,8 @@ from importlib.metadata import metadata
 
 class NetBoxTopologyConfig(PluginConfig):
     name = metadata("netbox_topology_plugin").get('Name').replace('-', '_')
-    verbose_name = metadata("netbox_topology_plugin").get('Summary')
-    description = "" #metadata("netbox_topology_plugin").get('Description')
+    verbose_name = "Netbox Topology Plugin"
+    description = metadata("netbox_topology_plugin").get('Summary') #metadata("netbox_topology_plugin").get('Description')
     version = metadata("netbox_topology_plugin").get('Version')
     author = metadata("netbox_topology_plugin").get("Author-email")
     author_email = metadata("netbox_topology_plugin").get("Author-email")
